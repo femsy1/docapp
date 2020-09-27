a5dbf008fabf12f8e362ff7ddd7b5bbf7ee28ed4
femsy1 <orakindipe@gmail.com>
2020-09-26 23:21:45 -0400

    add rds endpoint

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     // If you have not set database password on localhost then set empty.
     //  $conn = new mysqli($host, $user, $pass, $db_name);
     //    $con = mysqli_connect($host, $user, $pass, $db_name);
-        $con = mysqli_connect("docappdb-1.ceikcu3a9skn.us-east-1.rds.amazonaws.com","docappweb","Student123>","LoginSystem");
+        $con = mysqli_connect("idocappdb-1.ceikcu3a9skn.us-east-1.rds.amazonaws.com","docappweb","Student123>","LoginSystem");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
